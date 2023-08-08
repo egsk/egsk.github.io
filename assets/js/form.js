13a066558e813bf75f909f92d9b1729a7898c8ac
easkorokhodov <easkorokhodov@avito.ru>
2023-08-08 19:59:48 +0300

    fix form

---
+++ assets/js/form.js
@@ -0,0 +1,31 @@
+document.getElementById('feedback_form').addEventListener('submit', function(event) {
+    event.preventDefault()
+    this.querySelector('.error-message').classList.remove('d-block')
+    this.querySelector('.loading').classList.add('d-block')
+    var request = {
+        name: document.getElementById('input-name').value,
+        phone: document.getElementById('input-phone').value,
+        service: document.getElementById('input-service').value,
+        message: document.getElementById('input-message').value,
+    }
+
+    fetch(":6788", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        redirect: "follow",
+        body: JSON.stringify(request)
+    }).then(function() {
+        this.querySelector('.loading').classList.remove('d-block')
+        this.querySelector('.error-message').classList.remove('d-block')
+        this.querySelector('.sent-message').classList.add('d-block')
+    }.bind(this)).catch(function () {
+        this.querySelector('.loading').classList.remove('d-block')
+        this.querySelector('.error-message').classList.add('d-block')
+    }.bind(this))
+    
+    
+    
+    console.log(request)
+})
